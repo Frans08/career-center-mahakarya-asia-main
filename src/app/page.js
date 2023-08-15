6a0828c7029dd8f5167a034e5edf6711dd2ff247
Frans08 <fransfebry24@gmail.com>
2023-08-15 20:04:28 +0700

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-import HeroSection from "@/sections/HeroSection";
-import KelebihanSection from "@/sections/KelebihanSection";
-import HiringPartnersSection from "@/sections/HiringPartnersSection";
-import WelcomeSection from "@/sections/WelcomeSection";
-import PricingSections from "@/sections/PricingSections";
-import KelasSection from "@/sections/KelasSection";
-import JobConnectorSection from "@/sections/JobConnectorSection";
-import TestimoniSection from "@/sections/TestimoniSection";
-import FaqSection from "@/sections/FaqSection";
-import ContactSection from "@/sections/ContactSection";
+import HeroSection from "@/sections/homepage/HeroSection";
+import KelebihanSection from "@/sections/homepage/KelebihanSection";
+import HiringPartnersSection from "@/sections/homepage/HiringPartnersSection";
+import WelcomeSection from "@/sections/homepage/WelcomeSection";
+import PricingSections from "@/sections/homepage/PricingSections";
+import KelasSection from "@/sections/homepage/KelasSection";
+import JobConnectorSection from "@/sections/homepage/JobConnectorSection";
+import TestimoniSection from "@/sections/homepage/TestimoniSection";
+import FaqSection from "@/sections/homepage/FaqSection";
+import ContactSection from "@/sections/homepage/ContactSection";
 
 export default function Home() {
   return (
